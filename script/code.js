480cabf745144537ceafcbad84d54e8d399c80dc
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-17 10:28:53 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,25 @@
+let person1 = {
+    name: 'Ganiefah',
+    age: 18
+    };
+    //object 1
+    //question 1
+
+
+    let person2 = {
+        name: 'Yahred',
+        age: 18
+    };
+    //object 2
+    //question 2
+
+    console.log(person1);
+    console.log(person2);
+    //question 3
+    //will display data 
+
+    let data = [person1, person2];
+    console.log(data)
+
+
+
